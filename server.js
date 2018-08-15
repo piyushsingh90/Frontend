60c19f2451676907242bbde99ef9207760c52a72
Piyush <piyushsingh90@outlook.in>
2018-08-15 21:29:55 +0530

    Adding port

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/frontend'));
 
 app.get('/*', function(req,res) {
